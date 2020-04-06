b3a17f5c0b067c08de21132661eaca58d56693d0
Hok Lun Siu (Marco) <marco698698@hotmail.com>
2020-04-06 22:40:33 +0000

    clean some code up

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
 
   validates :first_name, :last_name, presence: true
   validates_uniqueness_of :email, case_sensitive: false, presence: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
   validates :password, length: { minimum: 3 }
   
   def self.authenticate_with_credentials(email, password)
